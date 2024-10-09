2532614821bf52178780a94f80e8f8d2a43fc5cf
“Shivanni <shivannirambaran@gmail.com>
2024-10-08 21:51:19 -0400

    Changed signIn from class to method to be uniform, commented out original to reference just in case.

--- ClickListener.java
+++ ClickListener.java
@@ -39,6 +39,34 @@ public class ClickListener implements ActionListener {
             }
         } else if (command.equals("Sign in")) {
             System.out.print("Sign in");
+        	//System.out.print("Create Account");
+        	parentFrame.getContentPane().removeAll();
+        	JPanel panel2 = new JPanel();
+        	JLabel labelUser = new JLabel("Which account would you like to create?");
+        	panel2.add(labelUser);
+        	
+   		 	JButton button1_1= new JButton("User 1 - Register Vehicle"); //1_1 suggests that it is related to button 1 which is create account so these buttons show up if you click button 1
+   		 	panel2.add(button1_1);
+   		 	button1_1.addActionListener(e -> GUI.createUser1(parentFrame));
+   		 	JButton button1_2=new  JButton ("User 2 - Use Vehicle");
+   		 	JButton button1_3 = new JButton ("User 3 - Cloud Controller");
+
+         // Add buttons to the panel
+   		 	panel2.add(button1_2);
+   		 	panel2.add(button1_3);
+
+         // Re-add the panel to the frame
+   		 	parentFrame.add(panel2);
+   		 	parentFrame.revalidate();  // Refresh the frame to show new content
+   		 	parentFrame.repaint();  // Repaint the frame
+        
+            
+        } 
+        else if (command.equals("Sign in")) {
+        	 System.out.print("Sign in");
+        	 parentFrame.getContentPane().removeAll();
+ 
+        	 
         }
     }
 
