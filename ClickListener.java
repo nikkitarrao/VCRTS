a2831784b6d1ab185940bc21a8a10934c3e75c29
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2024-10-08 21:14:53 -0400

    Add files via upload
    I changed the GUI class and the clicklistener class, and I created a method for createAccount() and for cloudController()

--- ClickListener.java
+++ ClickListener.java
@@ -1,52 +1,154 @@
+import java.awt.CardLayout;
+import java.util.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import javax.swing.*;
 
 public class ClickListener implements ActionListener {
-    private JFrame parentFrame;
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
 
-    // Constructor to initialize the parent frame
-    public ClickListener(JFrame parentFrame) {
-        this.parentFrame = parentFrame;
+    // Constructor to initialize the main panel and layout
+    public ClickListener(JPanel mainPanel, CardLayout cardLayout) {
+        this.mainPanel = mainPanel;
+        this.cardLayout = cardLayout;
     }
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        String command = event.getActionCommand();
-
+    	//getActionCommand method returns the name associated with the event (i.e. the name of the button, etc.)
+        String command = event.getActionCommand(); 
+        //if the create account button is clicked
         if (command.equals("Create an Account")) {
-        	//System.out.print("Create Account");
-        	parentFrame.getContentPane().removeAll();
-        	JPanel panel2 = new JPanel();
-        	JLabel labelUser = new JLabel("Which account would you like to create?");
-        	panel2.add(labelUser);
-        	
-   		 	JButton button1_1= new JButton("User 1 - Register Vehicle"); //1_1 suggests that it is related to button 1 which is create account so these buttons show up if you click button 1
-   		 	panel2.add(button1_1);
-   		 	button1_1.addActionListener(e -> GUI.createUser1(parentFrame));
-   		 	JButton button1_2=new  JButton ("User 2 - Use Vehicle");
-   		 	JButton button1_3 = new JButton ("User 3 - Cloud Controller");
-
-         // Add buttons to the panel
-   		 	//panel2.add(button1_1);
-   		 	panel2.add(button1_2);
-   		 	panel2.add(button1_3);
-
-         // Re-add the panel to the frame
-   		 	parentFrame.add(panel2);
-   		 	parentFrame.revalidate();  // Refresh the frame to show new content
-   		 	parentFrame.repaint();  // Repaint the frame
-        
-            
-        } 
-        else if (command.equals("Sign in")) {
-        	 //System.out.print("Sign in");
-        	 JPanel panelSignIn = new JPanel();
-        	 JLabel labelSignIn = new JLabel ("Please enter your login information");
-        	 
-        	 JButton button2_1 = new JButton("Sign in");
-        	 //button2_1.addActionListener(e -> GUI.createUser1(parentFrame)); ADD this section when you get lorena's part for the sign in
+        	//cardlayout will display the create account method display on the main panel
+            cardLayout.show(mainPanel, "CreateAccount");
+            if(command.equals("Cloud Controller")) {
+            	//cardlayout will display the cloud controller method display on the main panel
+            	cardLayout.show(mainPanel, "CloudController");
+            } else if(command.equals("Register a vehicle")) {
+            	//cardlayout will display the user1 method display on the main panel
+            	//cardLayout.show(mainPanel, "User1");
+            	} else {
+            		//cardlayout will display the user2 method display on the main panel
+            		//cardLayout.show(mainPanel, "User2");
+            }
+        } else if (command.equals("Sign in")) {
+            System.out.print("Sign in");
         }
     }
-}
 
+    public JPanel createAccount() {
+        JPanel createAccountPanel = new JPanel(); 
+        JLabel label = new JLabel("What type of account would you like to create?");
+        JButton button1 = new JButton("Register a vehicle");
+        JButton button2 = new JButton("Submit jobs");
+        JButton button3 = new JButton("Cloud Controller");
+        
+        //adding stuff to the panel
+        createAccountPanel.add(label);
+        createAccountPanel.add(button1);
+        createAccountPanel.add(button2);
+        createAccountPanel.add(button3);
+        
+      //event listener on user 1 button
+        button1.addActionListener(e -> {
+            cardLayout.show(mainPanel, "User1");
+        });
+        //event listener on user 2 button
+        button2.addActionListener(e -> {
+            cardLayout.show(mainPanel, "User2");
+        });
+
+        //event listener on cloud controller button
+        button3.addActionListener(e -> {
+            cardLayout.show(mainPanel, "CloudController");
+        });
+        //returning the panel so that it can be displayed when 'Create Account' button option is chosen 
+        return createAccountPanel;
+    }
+    
+    public JPanel cloudController() throws FileNotFoundException{
+    	//ArrayList to save cloud controller account information
+    	ArrayList <String> cloudControllerList = new ArrayList<String>();
+    	
+    	JPanel cloudControllerPanel = new JPanel();
+    	JLabel label = new JLabel("Create Cloud Controller Account- Authorized Users Only");
+    	JLabel label2 = new JLabel("Enter the following information below");
+    	JLabel adminCodeLabel = new JLabel("Admin Code: ");
+    	JTextField t1 = new JTextField(7);
+    	
+    	JTextField t2 = new JTextField(20);
+    	JTextField t3 = new JTextField(20);
+    	JTextField t4 = new JTextField(20);
+    	JTextField t5 = new JTextField(20);
+    	JLabel fnameLabel = new JLabel("First Name: ");
+    	JLabel lnameLabel = new JLabel("Last Name: ");
+    	JLabel emailLabel = new JLabel("Email: ");
+    	JLabel passwordLabel = new JLabel("Password: ");
+    	JButton submit = new JButton("Submit");
+    	
+    	 // File reading operations
+    	PrintStream output = new PrintStream(new File("out.txt"));
+       // Scanner input = new Scanner(System.in);
+    	
+    	cloudControllerPanel.add(label);
+    	cloudControllerPanel.add(label2);
+    	cloudControllerPanel.add(adminCodeLabel);
+    	cloudControllerPanel.add(t1);
+    	cloudControllerPanel.add(fnameLabel);
+    	cloudControllerPanel.add(t2);
+    	cloudControllerPanel.add(lnameLabel);
+    	cloudControllerPanel.add(t3);
+    	cloudControllerPanel.add(emailLabel);
+    	cloudControllerPanel.add(t4);
+    	cloudControllerPanel.add(passwordLabel);
+    	cloudControllerPanel.add(t5);
+    	cloudControllerPanel.add(submit);
+    	//line break to make more neat
+    	cloudControllerPanel.setLayout(new BoxLayout(cloudControllerPanel, BoxLayout.Y_AXIS)); 
+
+    	//event listener on submit button 
+        submit.addActionListener(e -> {
+        	
+        	//retrieves the info from the input fields 
+        	String adminCode = t1.getText();
+        	String fname = t2.getText();
+        	String lname = t3.getText();
+        	String email = t4.getText();
+        	String password = t5.getText();
+        	
+        	//doesnt allow you to create repeat accounts
+        	for(int i = 0; i < cloudControllerList.size(); i++) {
+        		if(adminCode.equals(cloudControllerList.get(i))) {
+            		
+            	}
+        	}
+        	
+        	
+        	//save it into the arraylist
+        	cloudControllerList.add(adminCode);
+        	cloudControllerList.add(fname);
+        	cloudControllerList.add(lname);
+        	cloudControllerList.add(email);
+        	cloudControllerList.add(password);
+        	
+        	//prints info gathered to printstream output folder
+        	output.print("Admin Code: " + adminCode + ", ");
+        	output.print("First Name: " + fname + ", ");
+        	output.print("Last Name: " + lname + ", ");
+        	output.print("Email: " + email + ", ");
+        	output.print("Password: " + password + ", ");
+        	
+        	
+        	// returns to the welcome page
+            cardLayout.show(mainPanel, "Welcome");
+        });
+    	return cloudControllerPanel;
+    		}
+
+}
--- GUI.java
+++ GUI.java
@@ -1,248 +1,46 @@
 import javax.swing.*;
-import java.util.*;
-import java.io.*;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-
-class SignIn extends JFrame implements ActionListener {
-    //initialize button, panel, label, and text field
-    JButton b1;
-    JPanel newPanel;
-    JLabel userLabel;
-    JLabel passLabel;
-    final JTextField textField1, textField2;
-
-    public SignIn() {
-        //Creating a label for email
-        userLabel = new JLabel();
-        userLabel.setText("Email: ");
-        textField1 = new JTextField(75); //set length of the text
-
-        passLabel = new JLabel();
-        passLabel.setText("Password");
-        textField2 = new JPasswordField(30);
-
-        b1 = new JButton("SUBMIT"); // set label to button
-
-        //create panel to put in form elements
-        newPanel = new JPanel(new GridLayout(3,1));
-        newPanel.add(userLabel);
-        newPanel.add(textField1);
-        newPanel.add(passLabel);
-        newPanel.add(textField2);
-        newPanel.add(b1);
-
-        //set border to panel
-        add(newPanel, BorderLayout.CENTER);
-
-        b1.addActionListener(this);
-        setTitle("SIGN IN FORM");
-        setSize(300, 150);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Allows the frame to be closed without exiting the app
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        String userValue = textField1.getText();
-        String passValue = textField2.getText();
-
-        if(!userValue.isEmpty() && !passValue.isEmpty()) {
-            JFrame page = new JFrame();
-            page.setSize(300, 150);
-            page.setTitle("Welcome");
-            JLabel welLabel = new JLabel("Welcome: " + userValue);
-            page.add(welLabel);
-            page.setVisible(true);
-        } else {
-            JOptionPane.showMessageDialog(this, "Please enter a valid username and password");
-        }
-    }
-}
-
-/*
- * private static void CreateAccount() {
-	// Create a new JFrame for create account
-}
-*/
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.awt.CardLayout;
 
 public class GUI {
-	
-	  public static void createUser1(JFrame frame) {
-		  ArrayList<User> user1List = new ArrayList<>();
-		// Clear the frame content to start fresh
-	        frame.getContentPane().removeAll();
+    public static void main(String[] args) throws FileNotFoundException {
+        JFrame frame = new JFrame("Vehicular Cloud Real-Time System Console");
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+       // welcomePanel.setLayout(new BoxLayout(welcomePanel, BoxLayout.Y_AXIS)); 
 
-	        // Create panel for users to register vehicles
-	        JPanel registerPanel = new JPanel();
-	        registerPanel.setLayout(new BoxLayout(registerPanel, BoxLayout.Y_AXIS)); //vertically aligns all the panels to make it more organized
-	        registerPanel.setSize(5, 10);
-	        
-	        // Create input fields for user info
-	        JTextField nameField = new JTextField(40);
-	        JTextField emailField = new JTextField(40);
-	        JTextField phoneField = new JTextField(40);
-	        JTextField userNameField = new JTextField(40);
-	        JPasswordField passwordField = new JPasswordField(40);
-	        JTextField makeField = new JTextField(40);
-	        JTextField modelField = new JTextField(40);
-	        JTextField yearField = new JTextField(4);
-	        JTextField vinField = new JTextField(40);
+        // Create the welcome panel
+        JPanel welcomePanel = new JPanel();
+        JLabel label = new JLabel("Welcome to the Vehicular Cloud Real-Time System Console");
+        JButton createAccountButton = new JButton("Create an Account");
+        JButton signInButton = new JButton("Sign in");
 
-	        // Add labels and input fields to the form
-	        registerPanel.add(new JLabel("Full Name:"));
-	        registerPanel.add(nameField);
-	        registerPanel.add(new JLabel("Email:"));
-	        registerPanel.add(emailField);
-	        registerPanel.add(new JLabel("Phone Number:"));
-	        registerPanel.add(phoneField);
-	        registerPanel.add(new JLabel("Username:"));
-	        registerPanel.add(userNameField);
-	        registerPanel.add(new JLabel("Password:"));
-	        registerPanel.add(passwordField);
-	        registerPanel.add(new JLabel("Car Make:"));
-	        registerPanel.add(makeField);
-	        registerPanel.add(new JLabel("Car Model:"));
-	        registerPanel.add(modelField);
-	        registerPanel.add(new JLabel("Car Year:"));
-	        registerPanel.add(yearField);
-	        registerPanel.add(new JLabel("VIN:"));
-	        registerPanel.add(vinField);
+        welcomePanel.add(label);
+        welcomePanel.add(createAccountButton);
+        welcomePanel.add(signInButton);
+        
+        // Add welcome panel to main panel
+        mainPanel.add(welcomePanel, "Welcome");
+        
+        // Create ClickListener and add the create account panel
+        ClickListener listener = new ClickListener(mainPanel, cardLayout);
+        mainPanel.add(listener.createAccount(), "CreateAccount");
+        mainPanel.add(listener.cloudController(), "CloudController"); // Add Cloud Controller panel
 
-	        // Submit button
-	        JButton submitButton = new JButton("Register");
-	        registerPanel.add(submitButton);
-	       
+        // Set up the frame
+        frame.add(mainPanel);
+        frame.setSize(400, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
-	        // Add action listener for submit button
-	        submitButton.addActionListener(e -> {
-	            // Gather data from input fields
-	        	// Check if any field is empty
-	            if (nameField.getText().isEmpty() ) {
-	                JOptionPane.showMessageDialog(frame, "Please enter your full name.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (emailField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid email address.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (phoneField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid phone number.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (userNameField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid username.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (passwordField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid password.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (makeField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid car make.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (modelField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid car model.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (yearField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid car year.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else if (vinField.getText().isEmpty()) {
-	                JOptionPane.showMessageDialog(frame, "Please enter a valid VIN.", "Error", JOptionPane.ERROR_MESSAGE);
-	            } 
-	            else {
-	                // If all fields are valid
-	                JLabel success = new JLabel("Vehicle registered, User Created!");
-	                registerPanel.add(success);
-	                registerPanel.revalidate();
-	                registerPanel.repaint();
-	        	
-	            String name = nameField.getText();
-	            String email = emailField.getText();
-	            String phone = phoneField.getText();
-	            String userName = userNameField.getText();
-	            String pw = passwordField.getText();
-	            String make = makeField.getText();
-	            String model = modelField.getText();
-	            int year = Integer.parseInt(yearField.getText());
-	            String vin = vinField.getText();
+        // Add action listeners
+        createAccountButton.addActionListener(listener);
 
-	            // Create a Vehicle object and a User object
-	            Vehicle vehicle = new Vehicle(make, model, year, vin);
-	            User user = new User(name, email, phone, userName, pw, vehicle);
-
-	            // Add the user to the list
-	            user1List.add(user);
-	            System.out.println("User information saved!");
-	            
-	            
-	           
-
-	            // Optionally clear the fields or reset the frame
-	            frame.getContentPane().removeAll();
-	            frame.revalidate();
-	            frame.repaint();
-	            }
-	        });
-
-	        // Add formPanel to the frame
-	        frame.add(registerPanel);
-	        frame.revalidate();
-	        frame.repaint();
-	        
-	    }
-
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		 JFrame frame = new JFrame();
-	      
-		 JLabel label = new JLabel("Welcome to the Vehicular Cloud Real-Time System Console");
-	      JButton button1 = new JButton("Create an Account");
-	      JButton button2 = new JButton("Sign in");
-
-	      JPanel panel = new JPanel();
-	      panel.add(label);
-	      panel.add(Box.createVerticalGlue()); //leaves a vertical space
-	      panel.add(Box.createVerticalGlue());
-	      panel.add(button1);
-	      panel.add(Box.createVerticalGlue());
-	      panel.add(Box.createVerticalGlue());
-	      panel.add(button2);
-	      frame.add(panel);
-
-	      final int FRAME_WIDTH = 400;
-	      final int FRAME_HEIGHT = 500;
-	      frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-	      frame.setTitle("Vehicular Cloud Real Time System");
-	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	      frame.setVisible(true);
-	      
-
-	        // ActionListener to show SignIn form only when "Sign in" button is clicked
-	        button2.addActionListener(new ActionListener() {
-	            @Override
-	            public void actionPerformed(ActionEvent e) {
-	                SignIn signInForm = new SignIn();
-	                signInForm.setVisible(true);
-	            }
-	        });
-	      
-	      ActionListener listener = new ClickListener(frame);
-	      button1.addActionListener(listener);
-	      button2.addActionListener(listener);
-	      
-	      //printing out the information
-	      File file = new File("data.txt");
-			
-			 Scanner input = new Scanner (file);
-			
-			while (input.hasNextDouble()) {
-	            double number = input.nextDouble();
-	            System.out.println(number);
-	        }
-			
-			
-			PrintStream output = new PrintStream(new File("out.txt"));
-
-	}
-
+    }
 }
+
 
 
